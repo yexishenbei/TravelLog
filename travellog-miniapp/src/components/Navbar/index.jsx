fa9af5290f8f7fade295575dba29e8ec00ad91d6
Roxie <geekhall.cn@gmail.com>
2025-05-13 19:40:09 +0800

    feat: 添加了搜索功能

--- travellog-miniapp/src/components/Navbar/index.jsx
+++ travellog-miniapp/src/components/Navbar/index.jsx
@@ -1,67 +1,19 @@
 import { View, Text, Image } from "@tarojs/components";
-import "./index.scss";
 import Taro from "@tarojs/taro";
+import "./index.scss";
 
-const Navbar = ({ title, showBack = true, onShare }) => {
-  // 处理搜索点击事件
+const Navbar = () => {
+  // 搜索按钮点击事件，跳转到搜索页面
   const handleSearchClick = () => {
-    Taro.showToast({
-      title: "搜索功能开发中",
-      icon: "none",
+    Taro.navigateTo({
+      url: "/pages/search/index",
     });
-  };
-
-  // 处理返回点击事件
-  const handleBackClick = () => {
-    try {
-      // 获取当前页面栈
-      const pages = Taro.getCurrentPages();
-      if (pages && pages.length > 1) {
-        Taro.navigateBack(); // 后退到上一页
-      } else {
-        Taro.showToast({
-          title: "没有上一页",
-          icon: "none",
-        });
-      }
-    } catch (error) {
-      Taro.showToast({
-        title: "获取页面栈失败",
-        icon: "none",
-      });
-    }
-  };
-
-  // 获取当前页面栈的长度，决定是否显示返回按钮
-  const shouldShowBack = () => {
-    if (!showBack) return false;
-    try {
-      const pages = Taro.getCurrentPages();
-      return pages && pages.length > 1;
-    } catch (error) {
-      return false;
-    }
   };
 
   return (
     <View className="navbar">
-      <View className="left">
-        {shouldShowBack() && (
-          <View className="back" onClick={handleBackClick}>
-            <Text className="icon">←</Text>
-          </View>
-        )}
-      </View>
-      <View className="center">
-        <Image className="logo" src="https://picsum.photos/50/50" />
-        <Text className="title">{title}</Text>
-      </View>
-      <View className="right">
-        {onShare && (
-          <View className="share" onClick={onShare}>
-            <Text className="icon">↗</Text>
-          </View>
-        )}
+      <View className="navbar-search" onClick={handleSearchClick}>
+        搜索
       </View>
     </View>
   );
