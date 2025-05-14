88867ea1e0cd528990e53eb6c4f664728d702711
lyy010720 <lyy_010720@163.com>
2025-05-14 13:59:13 +0800

    feat: 鉴权显示

--- my-app/src/components/aside/index.js
+++ my-app/src/components/aside/index.js
@@ -10,27 +10,34 @@ const { Sider } = Layout
 
 const Aside = props => {
   const { collapsed, Collapsed } = props
-
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const location = useLocation()
 
-  //添加数据到store
+  // 添加数据到 store
   const setTagList = val => {
     dispatch(selectTagList(val))
   }
 
-  //动态获取icon
+  // 获取当前用户角色
+  const userRole = localStorage.getItem('role'); // 假设角色信息保存在localStorage中
+
+  // 动态获取icon
   const iconToElement = name => React.createElement(Icon[name])
 
-  //菜单数据处理
+  // 菜单数据处理
   const getItems = (items, list = []) => {
-    items.map(item => {
+    items.forEach(item => {
+      // 如果是普通用户，过滤掉“回收站”菜单项
+      if (userRole !== 'admin' && item.label === '回收站') {
+        return; // 跳过回收站菜单项
+      }
+
       list.push({
         key: item.path,
         ...(item.icon ? { icon: iconToElement(item?.icon) } : []),
         label: item.label,
-        ...(item.children ? { children: getItems(item.children) } : [])
+        ...(item.children ? { children: getItems(item.children) } : []),
       })
     })
     return list
@@ -41,7 +48,7 @@ const Aside = props => {
     menuList.forEach(item => {
       if (item.path === e.keyPath[e.keyPath.length - 1]) {
         data = item
-        //如果有二级菜单
+        // 如果有二级菜单
         if (e.keyPath.length > 1) {
           data = item.children.find(child => {
             return child.path === e.key
@@ -52,7 +59,7 @@ const Aside = props => {
     setTagList({
       path: data.path,
       name: data.name,
-      label: data.label
+      label: data.label,
     })
     navigate(e.key)
   }
@@ -64,14 +71,15 @@ const Aside = props => {
         theme='dark'
         mode='inline'
         defaultSelectedKeys={['1']}
-        items={getItems(menuList)}
+        items={getItems(menuList)}  // 传入过滤后的菜单项
         selectedKeys={[location.pathname]}
         style={{
-          height: '100%'
+          height: '100%',
         }}
         onClick={clickMenu}
       />
     </Sider>
   )
 }
+
 export default Aside
